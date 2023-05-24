b576f6bdf8bbc77c7ab5d3a6d13ecab3e48231cb
Rockaide <vinirocca8@gmail.com>
2023-05-24 20:43:58 -0300

    Alterações_Multitarefas
    Alterações feitas para conclusão da tarefa passada.

--- MULTITAREFAS/MULTITAREFAS_R21/src/main.c
+++ MULTITAREFAS/MULTITAREFAS_R21/src/main.c
@@ -40,6 +40,7 @@ void tarefa_5(void);
 void tarefa_6(void);
 void tarefa_7(void);
 void tarefa_8(void);
+void new_task(void);
 
 /*
  * Configuracao dos tamanhos das pilhas
@@ -53,6 +54,7 @@ void tarefa_8(void);
 #define TAM_PILHA_7			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_8			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_OCIOSA	(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_NEW_TASK	(TAM_MINIMO_PILHA + 24)
 
 /*
  * Declaracao das pilhas das tarefas
@@ -66,6 +68,7 @@ uint32_t PILHA_TAREFA_6[TAM_PILHA_6];
 uint32_t PILHA_TAREFA_7[TAM_PILHA_7];
 uint32_t PILHA_TAREFA_8[TAM_PILHA_8];
 uint32_t PILHA_TAREFA_OCIOSA[TAM_PILHA_OCIOSA];
+uint32_t PILHA_NEW_TASK[TAM_PILHA_NEW_TASK];
 
 /*
  * Funcao principal de entrada do sistema
@@ -80,6 +83,8 @@ int main(void)
 	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 1);
 	
 	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 2);
+	
+	CriaTarefa(new_task, "New Task", PILHA_NEW_TASK, TAM_PILHA_NEW_TASK, 3);
 	
 	/* Cria tarefa ociosa do sistema */
 	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
@@ -104,7 +109,7 @@ void tarefa_1(void)
 	{
 		a++;
 		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE); /* Liga LED. */
-		TarefaContinua(2);
+		TarefaContinua(3);
 	
 	}
 }
@@ -117,6 +122,27 @@ void tarefa_2(void)
 		b++;
 		TarefaSuspende(2);	
 		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE); 	/* Turn LED off. */
+	}
+}
+
+void new_task(void)
+{
+	volatile uint16_t c = 0;
+	for(;;)
+	{
+		c++;
+		
+		//Do smth
+		
+		for(int i = 0; i < 10; i++){
+			/* Liga LED. */
+			port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
+			TarefaEspera(100); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
+			
+			/* Desliga LED. */
+			port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
+			TarefaEspera(100); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
+		}
 	}
 }
 
